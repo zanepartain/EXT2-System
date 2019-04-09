d3b177d6a90580fe70231ba963467f8175ae7c39
zpartain <zpartain@eecs.wsu.edu>
2019-04-09 09:49:47 -0700

    Update mkdir_rmdir.c -- implemented: ialloc, balloc, and dec_inc_freeBLOCKS. Started working on my_mkdir which the client will not interact with.

--- mkdir_rmdir.c
+++ mkdir_rmdir.c
@@ -16,13 +16,20 @@ extern char   line[256], cmd[32], pathname[256];
 #define OTHER  000007
 
 /**
- * Test and Set bits in buf 
+ * Test, Set, and Clear bits in buf 
  */
 int test_bit(char *buf, int bit){
-    return buf[bit/8] & (1 << (bit % 8));
+    if( buf[bit/8] & (1 << (bit % 8)))
+    {
+        return 1;
+    }
+    return 0;
 }
 int set_bit(char *buf, int bit){
     buf[bit/8] |= (1 << (bit % 8));
+}
+int clear_bit(char *buf,int bit){
+    buf[bit/8] &= ~(1 << (bit % 8));
 }
 
 
@@ -30,7 +37,7 @@ int set_bit(char *buf, int bit){
 * Decrement or increment the free blocks count in both the
 * SUPER block and GROUP_DESCRIPTOR block.
 */
-int dec_inc_freeINODES(int dev, char action){
+int dec_inc_freeBLOCKS(int dev, char action){
     char   buf[BLKSIZE];
     SUPER *super;
     GD    *group_descriptor;
@@ -72,13 +79,88 @@ int dec_inc_freeINODES(int dev, char action){
 
 
 /**
- * 
+* Decrement or increment the free INODES count in both the
+* SUPER block and GROUP_DESCRIPTOR block.
+*/
+int dec_inc_freeINODES(int dev, char action){
+    char   buf[BLKSIZE];
+    SUPER *super;
+    GD    *group_descriptor;
+
+    get_block(dev,1,buf);  //get super block
+    super = (SUPER *)buf;
+
+    switch (action)
+    {
+        case '+':
+            //put super block
+            super->s_free_inodes_count++;
+            put_block(dev,1,buf);
+            
+            //get and put group descriptor block
+            get_block(dev,2,buf);  
+            group_descriptor = (GD *)buf;
+            group_descriptor->bg_free_inodes_count++;
+            put_block(dev,2,buf);
+            break;
+
+        case '-':
+            //put super block
+            super->s_free_inodes_count--;
+            put_block(dev,1,buf);
+            
+            //get and put group descriptor block
+            get_block(dev,2,buf);  
+            group_descriptor = (GD *)buf;
+            group_descriptor->bg_free_inodes_count--;
+            put_block(dev,2,buf);
+            break;
+
+        default:
+            printf("_err: Invalid action ; dec_inc_freeINODES");
+            break;
+    }
+}
+
+/**
+ * allocate new Data Block
  */
-int ialloc(int dev){
+int balloc(int device){
     int iter;
     char buf[BLKSIZE];
 
-    
+    //read block_bitmap block
+    get_block(device, bmap, buf);
+
+    for(iter = 0; iter < ninodes; iter++){
+        if(test_bit(buf,iter) == 0){
+            set_bit(buf,iter);
+            put_block(device,imap,buf);
+            dec_inc_freeBLOCKS(device, '-');
+            return iter+1;
+        }
+    }
+}
+
+
+/**
+ * allocate a new INODE
+ */
+int ialloc(int device){
+    int iter;
+    char buf[BLKSIZE];
+
+    //read inode_bitmap block
+    get_block(device, imap, buf);
+
+    for(iter = 0; iter < ninodes; iter++){
+        if(test_bit(buf,iter) == 0){
+            set_bit(buf,iter);
+            put_block(device,imap,buf);
+            dec_inc_freeINODES(device, '-');
+            return iter+1;
+        }
+    }
 }
 
 
@@ -106,6 +188,17 @@ int dirname_basename(char *dirname, char *basename){
     return basename;
 }
 
+int my_mkdir(MINODE *pmip, char *child_name){
+    int ino, bno;
+
+    ino = ialloc(dev);    //get free inode#
+    bno = balloc(dev);    //get free block#
+    
+    printf("ino: %d\n", ino);
+    printf("bno: %d\n", bno);
+
+}
+
 
 /**
 * Makes a new child DIR inside of the specified parent DIR.
@@ -125,11 +218,13 @@ int make_directory(){
     {
         //get cwd inode#
         get_myino(running->cwd,&pino); 
+        dev = running->cwd->dev;
     }
     else
     {
         //get parent inode# from dirname 
-        pino = getino(dirname);        
+        pino = getino(dirname);   
+        dev = root->dev;     
     }
 
     pmip = iget(dev,pino);  //get parent MINODE
@@ -140,6 +235,7 @@ int make_directory(){
             //basename does not exist
             printf("basename = %s ; DOES NOT EXIST\t",basename);
             printf("dirname = %s \n",dirname);
+            my_mkdir(pmip,basename);
         }
         else{
             //basename already exists
