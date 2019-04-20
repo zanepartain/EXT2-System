647eb1c512402c441863ce379d621e32e3ca9a39
zpartain <zpartain@eecs.wsu.edu>
2019-04-19 21:25:26 -0700

    Update read_write.c -- Implementing read_file() method. As of now, it has grabbed the specified file descriptor from the running PROC. As well as the MINODE of the open file descriptor.

--- read_write.c
+++ read_write.c
@@ -1,5 +1,5 @@
 /************* read_write.c file **************/
-
+#include "type.h";
 /**** globals defined in main.c file ****/
 extern MINODE minode[NMINODE];
 extern MINODE *root;
@@ -16,3 +16,20 @@ extern char   line[256], cmd[32], pathname[256];
 #define OTHER  000007
 
 #include "string.h"
+
+
+/**
+ * Reads n bytes from an opened file descriptor into a buffer
+ * area in user space.
+ */
+int read_file(int fd,char *buf, int nbytes){
+    OFT *ofd = running->fd[fd];    //get open file descriptor
+    MINODE *mip = ofd->mptr;       //get MINODE of open file descriptor
+    
+    //num of bytes read ; offset of READ file ; available bytes in file  
+    int byte_count = 0;               
+    int offset = ofd->offset;
+    int available = mip->INODE.i_size - offset;
+
+    
+}
