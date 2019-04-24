020726031ec12a6bc608bd97c6f721f5cd08f05f
zpartain <zpartain@eecs.wsu.edu>
2019-04-23 19:02:34 -0700

    Update cp.c -- mycp() method now verifies  that the source file is in fact a REG file before opening it for READ. mycp() method also will verify that the dest file is in fact a REG file (if it exists) before proceeding. If dest file DNE, it will create the new dest file.

--- cp.c
+++ cp.c
@@ -17,10 +17,56 @@ extern char   line[256], cmd[32], pathname[256];
 
 #include <string.h>
 
+
 /**
- * Copies the source file to a destination file. If desttination
+ * Copies the source file to a destination file. If destination
  * file DNE, then it will create a new file.
  */
 int mycp(char *source, char *dest){
+    char sbuf[BLKSIZE];   //source file buffer[]
+    int sino, dino;       //soucre & dest ino
+    int lsfd, ldfd;       //source & dest fd
+    int nbytes;           //number of bytes read from source
+    MINODE *smip, *dmip;   //source & dest MINODE's
 
+
+    /*Get the source MINODE. Verify it exists, and that it 
+    is a REG FILE. Then open source file for READ, then read file*/
+    sino = getino(source);
+
+    if(sino == 0){
+        printf("_err: (source) %s DNE\n", name[n-1]);
+        return;
+    }
+
+    //get source MINODE and verify is REG FILE
+    smip = iget(dev,sino); 
+    if(dir_or_file(smip) != 0){
+        printf("_err: %s is not a REG FILE\n", name[n-1]);
+        return;
+    }
+
+    //open source file for READ ; READ entire file into sbuf[]
+    lsfd = open_file(source, "R");  
+    nbytes = read_file(lsfd, sbuf, smip->INODE.i_size);
+
+
+    /*Get the dest MINODE. If doesnt exist ten create it as REG FILE.
+    Then open dest file for WRITE, then write  sbuf[] to file*/
+    dino = getino(dest); 
+
+    if(dino == 0){
+        creat_file(dest);        //create dest file
+        ldfd = open(dest, "W");  //open dest file for WRITE
+    }
+    else{
+        //get dest MINODE and verify is REG FILE
+        dmip = iget(dev,dino); 
+        if(dir_or_file(dmip) != 0){
+            printf("_err: (dest) %s is not a REG FILE\n", name[n-1]);
+            return;
+        }
+
+
+    }
 }
